94d16e27c001aeb59b453bec53b836226234bda7
SShekotihin <sshekotihindev@gmail.com>
2022-01-12 18:30:47 +0300

    specs

--- spec/redis_cache_spec.cr
+++ spec/redis_cache_spec.cr
@@ -3,8 +3,8 @@ require "./spec_helper"
 describe RedisCache do
   it "with custom settings" do
     client = Redis::PooledClient.new(
-      host: "localhost",
-      port: 6379,
+      host: ENV["REDIS_HOST"]? || "localhost",
+      port: ENV["REDIS_PORT"]? ? ENV["REDIS_PORT"].to_i : 6379,
       database: 0,
       pool_size: 10
     )
@@ -20,15 +20,79 @@ describe RedisCache do
     RedisCache.settings.prefix.should eq "redis_cache"
   end
 
-  describe "#fetch" do
-    # TODO: write specs
-  end
+  describe "methods" do
+    before_each do
+      redis = Redis.new(
+        host: ENV["REDIS_HOST"]? || "localhost",
+        port: ENV["REDIS_PORT"]? ? ENV["REDIS_PORT"].to_i : 6379,
+        database: 0
+      )
 
-  describe "#read" do
-    # TODO: write specs
-  end
+      RedisCache.configure do |config|
+        config.redis = redis
+        config.fetch_with_cache = true
+      end
 
-  describe "#write" do
-    # TODO: write specs
+      redis.flushdb
+    end
+
+    describe "#fetch" do
+      it "writes and returns data" do
+        result = RedisCache.fetch(key: "writes and returns data") do
+          [1, 2, 3, 4, 5].to_s
+        end
+
+        RedisCache.read("writes and returns data").should eq result
+      end
+
+      it "key have ttl" do
+        RedisCache.fetch(key: "key have ttl", ttl: 10) { "expirable value" }
+
+        Redis.new(
+          host: ENV["REDIS_HOST"]? || "localhost",
+          port: ENV["REDIS_PORT"]? ? ENV["REDIS_PORT"].to_i : 6379,
+          database: 0
+        ).ttl("#{RedisCache.settings.prefix}:key have ttl").should eq 10
+      end
+
+      it "does not write to Redis if fetch_with_cache = false" do
+        RedisCache.settings.fetch_with_cache = false
+        RedisCache.fetch(key: "does not write to Redis if fetch_with_cache = false") { "some value" }
+
+        RedisCache.read(key: "does not write to Redis if fetch_with_cache = false").should eq nil
+      end
+
+      it "works with different types of key" do
+        RedisCache.fetch(key: [1, 2, 3, "foo", :bar]) {}
+        RedisCache.fetch(key: "string key") {}
+        RedisCache.fetch(key: :symbol_key) {}
+      end
+    end
+
+    describe "#write" do
+      it "key have ttl" do
+        RedisCache.write(key: "key have ttl", ttl: 10, value: "expirable value")
+
+        Redis.new(
+          host: ENV["REDIS_HOST"]? || "localhost",
+          port: ENV["REDIS_PORT"]? ? ENV["REDIS_PORT"].to_i : 6379,
+          database: 0
+        ).ttl("#{RedisCache.settings.prefix}:key have ttl").should eq 10
+      end
+
+      it "writes and returns data" do
+        value = [1, 2, 3, 4, 5].to_s
+        RedisCache.write(key: "writes and returns data", value: value)
+
+        RedisCache.read("writes and returns data").should eq value
+      end
+
+      it "writes data if fetch_with_cache = false" do
+        RedisCache.settings.fetch_with_cache = false
+        RedisCache.write(key: "writes data if fetch_with_cache = false", value: 1)
+
+        RedisCache.read(key: "writes data if fetch_with_cache = false").should eq "1"
+      end
+    end
   end
 end
